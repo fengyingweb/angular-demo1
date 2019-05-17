30d73610f068e588e186c8eb1bff9df42e371e6e
fengying <huangjun_web@163.com>
2019-05-17 14:27:16 +0800

    新增angular小demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './heroes/heroes.service';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+       <div class="container">
+          <div class="row">
+             <div class="col-lg-6 col-lg-offset-3">
+                <h1 class="text-center">{{ title }}</h1>
+                <h2> {{ hero.name }} details!</h2>
+             </div>
+          </div>
+       </div>
+     `,
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero= new Hero(1, 'Windstorm');
 }
---
+++ src/app/heroes/heroes.service.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by huangjun on 2017/10/18.
+ */
+// import { Injectable } from '@angular/core';
+
+export class Hero {
+  constructor (
+    public id: number,
+    public name: string
+  ) { }
+}
